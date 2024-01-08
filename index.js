d21e6aebc81a33545aa08725b9339a2ea57e94bf
aeamaea <amr.malik@gmail.com>
2024-01-08 09:42:32 -0500

    moved code around to make it clearer

--- index.js
+++ index.js
@@ -25,22 +25,25 @@ const doConversion = () => {
     // Start out with Unit values, Convert when user gives us something.
     let units = Number(unitInputEl.value) > 1 ? Number(unitInputEl.value) : 1
     // console.log(units)
-    // length 
+
+     // length 
     const m2f = (units * lenFactor).toFixed(3)
     const f2m = (units / lenFactor).toFixed(3)
-    // volume
-    const l2g = (units * volFactor).toFixed(3)
-    const g2l = (units / volFactor).toFixed(3)
-    // mass
-    const kg2lb = (units * massFactor).toFixed(3)
-    const lb2kg = (units / massFactor).toFixed(3) 
-
+     
     lengthEl.innerHTML = `
     <p>${units} Meter${s(units)} = ${m2f} Feet | ${units} F${units > 1 ? "ee" : "oo"}t = ${f2m} Meters</p>
     `
+     // volume
+    const l2g = (units * volFactor).toFixed(3)
+    const g2l = (units / volFactor).toFixed(3)
+     
     volumeEl.innerHTML = `
     <p>${units} Liter${s(units)} = ${l2g} Gallons | ${units} Gallon${s(units)} = ${g2l} Liters </p>
     `
+    // mass
+    const kg2lb = (units * massFactor).toFixed(3)
+    const lb2kg = (units / massFactor).toFixed(3) 
+     
     massEl.innerHTML = `
     <p>${units} Kilo${s(units)} = ${kg2lb} Pounds | ${units} Pound${s(units)} = ${lb2kg} Kilos</p>
     `
